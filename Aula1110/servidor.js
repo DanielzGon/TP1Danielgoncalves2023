1a003e428c26880f44130e9a248cd550abc23d87
DanielZGon <danielgoncalvescarvalho12@gmail.com>
2023-10-11 13:35:56 -0300

    kmnijni

--- Aula1110/servidor.js
+++ Aula1110/servidor.js
@@ -1,25 +1,39 @@
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (request, response) => {
     response.render("index.ejs")
 })
 
-app.get('/calcular', (req, res) => {
-    let n1 = parseFloat(req.query.txtValor1)
-    let n2 = parseFloat(req.query.txtValor2)
-    console.log("n1=" + req.query.txtValor1)
-    console.log("n2=" + req.query.txtValor2)
-    result = n1 + n2
-    if (isNaN(result)) {
-        result = "Valores inválidos."
-    } else {
-        result = `${n1} + ${n2} = ${result}`
-    }
+app.post('/calcular', (req, res) => {
+    let n1 = parseFloat(req.body.txtValor1)
+    let n2 = parseFloat(req.body.txtValor2)
+    console.log(req.body.operacoes);
+    let result
+if (req.body.operacoes == "-"){
+    result = `${n1} - ${n2} = ${n1-n2}`
+}
+else if (req.body.operacoes == "+"){
+     result = `${n1} + ${n2} = ${n1+n2}`
+}
+else if (req.body.operacoes == "*"){
+     rresult = `${n1} * ${n2} = ${n1*n2}`
+}
+else if (req.body.operacoes == "/"){
+    result = `${n1} / ${n2} = ${n1/n2}`
+}
+ else {
+    result = "Valores inválidos."
+    return
+ }   
+
     
-    res.render("index.ejs",{ saida: result })
-    document.getElementById("result").innerHTML = "Resultado: "+ result;
+    
+
+    res.render("index2.ejs",{ saida: result })
 })
 
 app.listen(8080)
