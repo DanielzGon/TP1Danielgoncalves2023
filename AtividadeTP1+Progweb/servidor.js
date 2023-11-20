916c74dafa67f35d09a1ad1d768c6cb4c73b743f
DanielzGon <danielgoncalvescarvalho12@gmail.com>
2023-11-20 16:52:00 -0300

    estrutura pronta, falta js e css

--- AtividadeTP1+Progweb/servidor.js
+++ AtividadeTP1+Progweb/servidor.js
@@ -1,16 +1,24 @@
-const port = 8080;
+const port = 8000;
 
 const express = require("express");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-app.listen(8080, ()=> console.log
-('server funcionando na porta: ', port))
+app.listen(8000, ()=> console.log
+('Servidor funcionando na porta: ', port))
 
 const cors = require('cors')
 app.use(cors())
 
 app.get ('/', (req, res) => {
     res.render('index')
+})
+
+app.get ('/informacoes', (req, res) => {
+    res.render('partials/informacoes')
+})
+
+app.get ('/cadastro', (req, res) => {
+    res.render('cadastro')
 })
