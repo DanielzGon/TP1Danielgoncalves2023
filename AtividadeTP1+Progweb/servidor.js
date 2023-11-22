00c4e3cec22ccbba18bf779d0d10bfd9e540bd50
kauanAg-devs <kauan.aguiar@gsuite.iff.edu.br>
2023-11-22 12:12:15 -0300

    js começado

--- AtividadeTP1+Progweb/servidor.js
+++ AtividadeTP1+Progweb/servidor.js
@@ -1,7 +1,10 @@
 const port = 4040;
 
 const express = require("express");
+const fs = require('fs')
+
 const app = express();
+app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
@@ -18,6 +21,20 @@ app.get ('/informacoes', (req, res) => {
 
 app.get ('/cadastro', (req, res) => {
     res.render('cadastro')
+})
+
+app.post('/salvar', (req, res) => {
+    let nomeNoForm = req.body.nome
+    // Salvando um nome (sobrescrevendo o arquivo)
+    //fs.writeFileSync('nome.txt', nomeNoForm)
+    // Salvando vários nomes
+    //fs.appendFileSync('nomes.txt', `\n${nomeNoForm}`)
+    let cadastro = {nome: nomeNoForm}
+    console.log(cadastro);
+    console.log('\n'+JSON.stringify(cadastro)+',');
+    fs.appendFileSync('nomes.json', `\n${JSON.stringify(cadastro)}`)
+    resultado = `Olá, ${nomeNoForm}`
+    res.render('index', { resultado })
 })
 
 app.listen(port, ()=> console.log ('Servidor funcionando na porta: ', port))
