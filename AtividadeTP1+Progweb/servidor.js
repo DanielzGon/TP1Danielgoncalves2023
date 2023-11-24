c1f19f92f375012472b5eb09a9fd92d96b0f7371
DanielZgon <danielgoncalvescarvalho12@gsuite.iff.edu.br>
2023-11-24 15:59:29 -0300

    Nomes no Json

--- AtividadeTP1+Progweb/servidor.js
+++ AtividadeTP1+Progweb/servidor.js
@@ -1,40 +1,42 @@
 const port = 4040;
 
 const express = require("express");
-const fs = require('fs')
+const fs = require("fs");
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
-app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-const cors = require('cors')
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
 
-app.get ('/', (req, res) => {
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get ('/informacoes', (req, res) => {
-    res.render('partials/informacoes')
-})
+app.get("/informacoes", (req, res) => {
+  res.render("partials/informacoes");
+});
 
-app.get ('/cadastro', (req, res) => {
-    res.render('cadastro')
-})
+app.get("/cadastro", (req, res) => {
+  res.render("cadastro");
+});
 
-app.post('/salvar', (req, res) => {
-    let nomeNoForm = req.body.nome
-    // Salvando um nome (sobrescrevendo o arquivo)
-    //fs.writeFileSync('nome.txt', nomeNoForm)
-    // Salvando vários nomes
-    //fs.appendFileSync('nomes.txt', `\n${nomeNoForm}`)
-    let cadastro = {nome: nomeNoForm}
-    console.log(cadastro);
-    console.log('\n'+JSON.stringify(cadastro)+',');
-    fs.appendFileSync('nomes.json', `\n${JSON.stringify(cadastro)}`)
-    resultado = `Olá, ${nomeNoForm}`
-    res.render('index', { resultado })
-})
+app.post("/salvar", (req, res) => {
+  let nomeNoForm = req.body.nome;
+  let sobrenomeNoForm = req.body.sobrenome;
+  let telefoneNoForm = req.body.telefone;
+  let cadastro = {
+    nome: nomeNoForm,
+    sobrenome: sobrenomeNoForm,
+    telefone: telefoneNoForm,
+  };
+  console.log(cadastro);
+  console.log("\n" + JSON.stringify(cadastro) + ",");
+  fs.appendFileSync("nomes.json", `\n${JSON.stringify(cadastro)}`);
+  resultado = `Olá, ${nomeNoForm}`;
+  res.render("salvar", { resultado });
+});
 
-app.listen(port, ()=> console.log ('Servidor funcionando na porta: ', port))
+app.listen(port, () => console.log("Servidor funcionando na porta: ", port));
